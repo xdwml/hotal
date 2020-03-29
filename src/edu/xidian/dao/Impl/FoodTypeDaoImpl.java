43f586c6d304080aca9f8e49ed9e60499e0f3d6a
wml <18270856781@163.com>
2020-03-29 22:36:01 +0800

    Signed-off-by: wml <18270856781@163.com>

---
+++ src/edu/xidian/dao/Impl/FoodTypeDaoImpl.java
@@ -0,0 +1,40 @@
+package edu.xidian.dao.Impl;
+
+import edu.xidian.dao.FoodTypeDao;
+import edu.xidian.entity.FoodType;
+
+import java.util.List;
+
+
+public class FoodTypeDaoImpl implements FoodTypeDao {
+
+    @Override
+    public void save(FoodType foodType) {
+
+    }
+
+    @Override
+    public void update(FoodType foodType) {
+
+    }
+
+    @Override
+    public void update(int id) {
+
+    }
+
+    @Override
+    public FoodType findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<FoodType> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<FoodType> getAll(String typeName) {
+        return null;
+    }
+}
---
+++ src/edu/xidian/entity/FoodType.java
@@ -0,0 +1,27 @@
+package edu.xidian.entity;
+
+/**
+ * 1.菜系模块，实体类设计，主要用于封装数据
+ */
+public class FoodType {
+    private int id;//类别主键
+    private String typeName;//类别名称
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+
+}
